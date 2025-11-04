4500c58b57cfc39e1a41e4630cc26ca3a3a726ce
Nguyen Giang <giang.nguyenhoang.cit21@eiu.edu.vn>
2025-11-04 18:57:10 +0700

    Test: Implement integration tests for Controller-Model interaction

--- tests/integration/controller.test.js
+++ tests/integration/controller.test.js
@@ -20,16 +20,58 @@ describe('Controller-Service Integration Tests', () => {
     });
 
     test('handleAddTodo should call service.addTodo and update the model', () => {
+        const testText = 'Buy groceries';
+
         // TODO: Call the controller's handleAddTodo method with some test text.
+        controller.handleAddTodo(testText);
+
         // Then, get the list of todos directly from the service.
+        const todos = service.getTodos();
+
         // Assert that the service's todos array has a length of 1.
+        expect(todos.length).toBe(1);
+
         // Assert that the text of the first todo in the service matches the input.
+        expect(todos[0].text).toBe(testText);
     });
 
     test('handleRemoveTodo should call service.removeTodo and update the model', () => {
+        const testText = 'Task to be removed';
+
         // TODO: First, directly add a todo to the service.
+        service.addTodo(testText);
+        
         // Get the ID of the new todo.
+        const todoId = service.getTodos()[0].id;
+        
+        // Assert initial state
+        expect(service.getTodos().length).toBe(1);
+
         // Call the controller's handleRemoveTodo method with that ID.
+        controller.handleRemoveTodo(todoId);
+
         // Assert that the service's todos array is now empty.
+        expect(service.getTodos().length).toBe(0);
+    });
+
+    // Thêm test cho handleToggleTodo để hoàn thiện
+    test('handleToggleTodo should call service.toggleTodoComplete and update the model', () => {
+        const testText = 'Task to be toggled';
+        
+        // Add a todo
+        service.addTodo(testText);
+        const todo = service.getTodos()[0];
+        const todoId = todo.id;
+        
+        // Assert initial state: should be false
+        expect(todo.completed).toBe(false);
+
+        // Toggle the todo (false -> true)
+        controller.handleToggleTodo(todoId);
+        expect(todo.completed).toBe(true);
+        
+        // Toggle again (true -> false)
+        controller.handleToggleTodo(todoId);
+        expect(todo.completed).toBe(false);
     });
 });
